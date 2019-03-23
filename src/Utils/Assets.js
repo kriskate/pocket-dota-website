8dfc3c3c7902e2e99ebb033459bc42a6c5530e6d
kristi <kristi.stanescu@gmail.com>
2019-03-23 11:22:21 +0200

    refactored assets loading/ location

--- src/Utils/Assets.js
+++ src/Utils/Assets.js
@@ -1,34 +1,27 @@
-import icon from '../images/app-icon-no-border.png';
-
-import badgeGoogle from '../images/badge-google-play.png';
-import badgeApple from '../images/badge-appstore.png';
-
-import image_home_framed from '../images/screenshots-android/home_framed.png';
-
-import image_home from '../images/screenshots-android/home.png';
-import image_heroes from '../images/screenshots-android/heroes.png';
-import video_hero from '../images/screenshots-android/hero.mp4';
-import image_items from '../images/screenshots-android/items.png';
-import image_item from '../images/screenshots-android/item.png';
-import image_stats from '../images/screenshots-android/stats.png';
-import image_stats_profile from '../images/screenshots-android/stats-profile.png';
-
-import image_BTC from '../images/crypto/BTC.png';
-import image_ETH from '../images/crypto/ETH.png';
-
-import logo_paypal from '../images/logos/paypalme.png';
-import logo_discord from '../images/logos/discord.svg';
-import logo_expo from '../images/logos/expo.png';
-import logo_bitcoin from '../images/crypto/logo-bitcoin.jpg';
-import logo_ethereum from '../images/crypto/logo-ethereum.png';
-
 export const Images = {
-  icon, 
-  badgeGoogle, badgeApple,
-  image_home_framed,
-  image_home, image_heroes, video_hero, image_item, image_items, image_stats, image_stats_profile,
-  image_BTC, image_ETH,
-  logo_paypal, logo_discord, logo_expo, logo_bitcoin, logo_ethereum,
+  icon: require('../images/app-icon-no-border.png'),
+  
+  badgeGoogle: require('../images/badges/badge-google-play.png'),
+  badgeApple: require('../images/badges/badge-appstore.png'),
+
+  image_home_framed: require('../images/screenshots-android/home_framed.png'),
+  image_home: require('../images/screenshots-android/home.png'),
+  image_heroes: require('../images/screenshots-android/heroes.png'),
+
+  video_hero: require('../images/screenshots-android/hero.mp4'),
+  image_item: require('../images/screenshots-android/items.png'),
+  image_items: require('../images/screenshots-android/item.png'),
+  image_stats: require('../images/screenshots-android/stats.png'),
+  image_stats_profile: require('../images/screenshots-android/stats-profile.png'),
+
+  image_BTC: require('../images/crypto/BTC.png'),
+  image_ETH: require('../images/crypto/ETH.png'),
+
+  logo_paypal: require('../images/logos/paypalme.png'),
+  logo_discord: require('../images/logos/discord.svg'),
+  logo_expo: require('../images/logos/expo.png'),
+  logo_bitcoin: require('../images/crypto/logo-bitcoin.jpg'),
+  logo_ethereum: require('../images/crypto/logo-ethereum.png'),
 }
 
 export const URLS = {
